0b422890971ccf36811ce767a43d5d35a05368dc
Lucky <lucky123@gmail.com>
2024-11-09 09:56:04 +0530

    logi page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/react-in-jsx-scope */
+import AuthForm from "../../../components/AurthFrom";
+
+
+
+const SignIn = () => <AuthForm type="sign-in" />;
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import AuthForm from "../../../components/AurthFrom";
+
+
+const SignUp = () => <AuthForm type="sign-up" />;
+
+export default SignUp;
